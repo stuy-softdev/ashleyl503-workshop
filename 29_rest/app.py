688070283b09b55de349b2009faafa456f338910
ashli64 <ali64@stuy.edu>
2025-11-21 10:03:23 -0500

    done code

--- 29_rest/app.py
+++ 29_rest/app.py
@@ -1,3 +1,25 @@
-# Ashley Li, Natalie Keiger
-# Awesome Sauce
-# 29_rest
+import urllib.request
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/")
+def main_page():
+
+    fi = open('key_nasa.txt', 'r')
+    key = fi.read()
+
+    with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=' + key) as response:
+        response = response.read().decode()
+        print(response)
+        json_obj = json.loads(response)
+        print(json_obj['url'])
+        img = json_obj['url']
+        desc = json_obj['explanation']
+
+    return render_template('main.html', image=img, exp=desc)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
