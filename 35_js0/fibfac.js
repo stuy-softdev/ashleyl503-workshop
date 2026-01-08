7ef959422a42300957a181ac5219f69143f46f96
ashli64 <ali64@stuy.edu>
2026-01-08 11:48:17 -0500

    fact and fib

---
+++ 35_js0/fibfac.js
@@ -0,0 +1,32 @@
+//Team Virginia :: Veronika Duvanova, Ashley Li
+//SoftDev pd5
+//K35 - Basic functions in JavaScript
+//2026-01-09f
+
+//JavaScript implementations of recursive Scheme functions
+
+//factorial:
+define fact(n) {
+  if (n == 0) {
+    return(0);
+  } else {
+    return(n * fact(n - 1));
+  }
+};
+//<your team's fact(n) implementation>
+
+//TEST CALLS
+// (writing here beforehand will facilitate EZer copy/pasting into dev console now and later...)
+console.log(fact(3));
+
+//-----------------------------------------------------------------
+
+
+//fib:
+
+//<your team's fib(n) implementation>
+
+//TEST CALLS
+// (writing here beforehand will facilitate EZer copy/pasting into dev console now and later...)
+
+//=================================================================
