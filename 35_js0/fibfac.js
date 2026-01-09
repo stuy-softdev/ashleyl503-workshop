e0baad642ebc9fa681b8e982e64142ddbac08de4
Ashley Li <ali64@stuy.edu>
2026-01-08 22:35:49 -0500

    Correct fib function implementation
    Fix the base case and recursive calls in the fib function.

--- 35_js0/fibfac.js
+++ 35_js0/fibfac.js
@@ -26,14 +26,16 @@ console.log(fact(3));
 
 //<your team's fib(n) implementation>
 define fib(n) {
-  if (n == 0) {
+  if (n <= 0) {
     return(0);
-  } else {
-    return(
-      fin (n - 1) + fib(n - !));
+  } if (n == 1) {
+    return(1);
+  }
+  else {
+    return(fib(n - 2) + fib(n - !));
   }
 };
 //TEST CALLS
 // (writing here beforehand will facilitate EZer copy/pasting into dev console now and later...)
-
+console.log(fib(3));
 //=================================================================
