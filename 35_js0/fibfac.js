bf5c07aa85116573964bf0df01095463c4a7ae5d
Ashley Li <ali64@stuy.edu>
2026-01-08 22:33:52 -0500

    Correct fib function implementation
    Fix the implementation of the fib function to correctly calculate Fibonacci numbers.

--- 35_js0/fibfac.js
+++ 35_js0/fibfac.js
@@ -25,7 +25,14 @@ console.log(fact(3));
 //fib:
 
 //<your team's fib(n) implementation>
-
+define fib(n) {
+  if (n == 0) {
+    return(0);
+  } else {
+    return(
+      fin (n - 1) + fib(n - !));
+  }
+};
 //TEST CALLS
 // (writing here beforehand will facilitate EZer copy/pasting into dev console now and later...)
 
